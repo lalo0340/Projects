0b80d97bb2f43541b0ad76e080cca759b377559a
lalo0340 <lalo0340@algonquinlive.com>
2014-01-17 15:16:07 -0500

    Version 1 Assignment 1

---
+++ GameProgramming_Assignment1/GameProgramming_Assignment1/Resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by GameProgramming_Assignment1.rc
+//
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_GAMEPROGRAMMING_ASSIGNMENT1_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_CLEAR				106
+#define IDM_EXIT				105
+#define IDM_RED				110
+#define IDM_BLUE				111
+#define IDM_GREEN				112
+#define IDM_PURPLE				113
+
+
+#define IDM_PEN				114
+#define IDM_ELLIPSE				115
+#define IDM_RECTANGLE		116
+
+#define IDI_GAMEPROGRAMMING_ASSIGNMENT1			107
+#define IDI_SMALL				108
+#define IDC_GAMEPROGRAMMING_ASSIGNMENT1			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
